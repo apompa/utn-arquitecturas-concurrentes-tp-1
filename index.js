8f372c67ad203ab291804d5c232167c7e48561c3
Nicolás Santángelo <santangelonicolas@gmail.com>
2015-09-01 20:40:50 -0300

    Server+ignore

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+var sendFile = function(fileName) {
+    return function(req, res) {
+      res.sendFile(__dirname + '/' + fileName + '.html');
+    };
+};
+
+app.get('/', sendFile('alumno'));
+app.get('/alumno', sendFile('alumno'));
+
+app.get('/profesor', sendFile('profesor'));
+
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    socket.on('student question', function(socket){
+        console.log('STUDENT');
+        io.emit('student question', msg);
+    });
+
+    socket.on('teacher answer', function(socket){
+        console.log('TEACHER');
+        io.emit('teacher answer', msg);
+    });
 });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
+//socket.on
+//socket.broadcast.emit
